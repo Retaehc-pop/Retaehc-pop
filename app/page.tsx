6c4a1cf68dbac950edbc2ed2052f7a2f0c751089
Retaehc-pop <papop2003@gmail.com>
2023-02-20 01:27:43 +0100

    banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { HypertextLoop } from '../components/hypertextloop'
+import PageBanner from '../components/pageBanner'
 const Page = () => {
   return (
     <>
     <main>
+      <PageBanner>Home</PageBanner>
       <HypertextLoop texts={["Pi"]} delay={3000}>
         Main Page
       </HypertextLoop>
---
+++ components/pageBanner.tsx
@@ -0,0 +1,25 @@
+"use client"
+import styles from "../components/Components.module.scss";
+import { useEffect, useState } from "react";
+const PageBanner = ({ children }: { children: string }) => {
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setMousePos({ x: window.innerWidth-e.clientX*2, y: e.clientY - mousePos.y*-2});
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div className={styles.pageBanner}>
+      <h1 style={{ transform: `translate(${mousePos.y/100}px,${ mousePos.x/100}px) ` }}>{children.toUpperCase()}</h1>
+      {/* <p>{mousePos.x*100+" "+mousePos.y*10}</p> */}
+    </div>
+  );
+};
+
+export default PageBanner;
