61a74782122e3b0535d46975e035c0ada9bb9f18
Retaehc-pop <papop2003@gmail.com>
2023-02-24 15:26:12 +0100

    restructure

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -2,29 +2,39 @@ import { NextPage } from "next";
 import styles from "./About.module.scss";
 import PageBanner from "../../components/pageBanner";
 import Skills from "../../container/skills";
+import Image from "next/image";
 const About: NextPage = () => {
   return (
     <>
-      <main>
-        <PageBanner>About</PageBanner>
+      <PageBanner>About</PageBanner>
+      <main className={styles.main}>
+        <div className={styles.pic}>
+          <Image src="/profile.jpg" alt="profile picture of Papop" fill style={{objectFit:'cover'}}/>
+        </div>
         <section>
-        <h3>Education</h3>
-        <p>Assumption College, Bangkok Thailand</p>
-        <p>RWTH Aachen University, Aachen Germany</p>
-        <p>B.Sc Computer Science</p>
+          <h3>Education</h3>
+          <div>
+            <p>2015-2022</p>
+            <p>Assumption College, Bangkok Thailand</p>
+          </div>
+          <div>
+            <p>2022-Present</p>
+            <p>RWTH Aachen University, Aachen Germany</p>
+            <p>B.Sc Computer Science</p>
+          </div>
         </section>
         <section>
           <h3>Experience</h3>
-          <p>2020</p>
-          <p>
-            SPACE AC institute of Technology Project Lead & Head of Programming
-            department
-          </p>
+          <div>
+            <h2>Technical Lead</h2>
+            <p>2020-2022</p>
+            <p>SPACE AC institute of Technology</p>
+          </div>
         </section>
-        <section>
+        {/* <section>
           <h3>Skills</h3>
-          <Skills/>
-        </section>
+          <Skills />
+        </section> */}
       </main>
     </>
   );
