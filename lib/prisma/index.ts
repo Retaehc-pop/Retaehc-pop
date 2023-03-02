a032ee67e5325d53502ed8edc927a8a50250a9dc
Retaehc-pop <papop2003@gmail.com>
2023-03-02 01:06:44 +0100

    add global prisma

---
+++ lib/prisma/index.ts
@@ -0,0 +1,38 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+
+
+declare global {
+  var prisma: PrismaClient;
+}
+
+
+let prisma: PrismaClient;
+//check if we are running in production mode
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  //check if there is already a connection to the database
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export type technologyWithPosition = Prisma.TechnologyGetPayload<{
+  include: {
+    position: true;
+  };
+}>;
+
+export type yearWithProjectpic = Prisma.YearGetPayload<{
+  include: {
+    project:{
+      select:{
+        name:true,
+        image:true,
+      }
+    }
+  }
+}>;
+
+export { prisma };
