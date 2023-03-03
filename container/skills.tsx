da897046c2fee79e28ea3915bad5e8233d1b7094
Retaehc-pop <papop2003@gmail.com>
2023-03-03 16:56:21 +0100

    cmt

--- container/skills.tsx
+++ container/skills.tsx
@@ -1,206 +1,35 @@
-import Arduino from "../public/svg/arduino.svg";
-import C from "../public/svg/c.svg";
-import Cpp from "../public/svg/cplusplus.svg";
-import Css from "../public/svg/css.svg";
-import Git from "../public/svg/git.svg";
-import Github from "../public/svg/github.svg";
-import Gitlab from "../public/svg/gitlab.svg";
-import Go from "../public/svg/go.svg";
-import Haskell from "../public/svg/haskell.svg";
-import Html from "../public/svg/html.svg";
-import Java from "../public/svg/java.svg";
-import Javascript from "../public/svg/javascript.svg";
-import Jupyter from "../public/svg/jupyter.svg";
-import Mysql from "../public/svg/mysql.svg";
-import Nextjs from "../public/svg/nextjs.svg";
-import Nodejs from "../public/svg/nodejs.svg";
-import Numpy from "../public/svg/numpy.svg";
-import Pandas from "../public/svg/pandas.svg";
-import Python from "../public/svg/python.svg";
-import Pytorch from "../public/svg/pytorch.svg";
-import React from "../public/svg/react.svg";
-import Rpi from "../public/svg/rpi.svg";
-import Rust from "../public/svg/rust.svg";
-import Sass from "../public/svg/sass.svg";
-import Solidity from "../public/svg/solidity.svg";
-import Tensorflow from "../public/svg/tensorflow.svg";
-import Firebase from "../public/svg/firebase.svg";
-import Typescript from "../public/svg/typescript.svg";
-import Mongodb from "../public/svg/mongodb.svg";
-
 export default [
-  {
-    name: "Haskell",
-    icon: <Haskell />,
-    experties: 3,
-    pos: { x: '1rem', y: '1rem' },
-  },
-  {
-    name: "Python",
-    icon: <Python />,
-    experties: 5,
-    pos: { x: '2rem', y: '-3rem' },
-  },
-  {
-    name: "Typescript",
-    icon: <Typescript />,
-    experties: 5,
-    pos: { x: '-6rem', y: '-4rem' },
-  },
-  {
-    name: "Javascript",
-    icon: <Javascript />,
-    experties: 5,
-    pos: { x: '-4rem', y: '-2rem' },
-  },
-  {
-    name: "C",
-    icon: <C />,
-    experties: 3,
-    pos: { x: '-4rem', y: '2rem' },
-  },
-  {
-    name: "C++",
-    icon: <Cpp />,
-    experties: 3,
-    pos: { x: '4rem', y: '2rem' },
-  },
-  {
-    name: "Rust",
-    icon: <Rust />,
-    experties: 1,
-    pos: { x: '5rem', y: '-1rem' },
-  },
-  {
-    name: "Go",
-    icon: <Go />,
-    experties: 1,
-    pos: { x: '-2rem', y: '-1rem' },
-  },
-  {
-    name: "Java",
-    icon: <Java />,
-    experties: 3,
-    pos: { x: '-2rem', y: '-4rem' },
-  },
-  {
-    name: "Html",
-    icon: <Html />,
-    experties: 5,
-    pos: { x: '-2rem', y: '4rem' },
-  },
-  {
-    name: "Css",
-    icon: <Css />,
-    experties: 3,
-    pos: { x: '0rem', y: '4rem' },
-  },
-  {
-    name: "Sass",
-    icon: <Sass />,
-    experties: 5,
-    pos: { x: '0rem', y: '-4rem' },
-  },
-  {
-    name: "React",
-    icon: <React />,
-    experties: 3,
-    pos: { x: '2rem', y: '4rem' },
-  },
-  {
-    name: "Nextjs",
-    icon: <Nextjs />,
-    experties: 5,
-    pos: { x: '4rem', y: '4rem' },
-  },
-  {
-    name: "Nodejs",
-    icon: <Nodejs />,
-    experties: 3,
-    pos: { x: '6rem', y: '4rem' },
-  },
-  {
-    name: "Git",
-    icon: <Git />,
-    experties: 2,
-    pos: { x: '8rem', y: '4rem' },
-  },
-  {
-    name: "Github",
-    icon: <Github />,
-    experties: 3,
-    pos: { x: '8rem', y: '2rem' },
-  },
-  {
-    name: "Gitlab",
-    icon: <Gitlab />,
-    experties: 0,
-    pos: { x: '8rem', y: '0rem' },
-  },
-  {
-    name: "Arduino",
-    icon: <Arduino />,
-    experties: 4,
-    pos: { x: '8rem', y: '-4rem' },
-  },
-  {
-    name: "Raspberry Pi",
-    icon: <Rpi />,
-    experties: 4,
-    pos: { x: '8rem', y: '-2rem' },
-  },
-  {
-    name: "Solidity",
-    icon: <Solidity />,
-    experties: 1,
-    pos: { x: '-4rem', y: '-4rem' },
-  },
-  {
-    name: "Tensorflow",
-    icon: <Tensorflow />,
-    experties: 4,
-    pos: { x: '-4rem', y: '4rem' },
-  },
-  {
-    name: "Pytorch",
-    icon: <Pytorch />,
-    experties: 2,
-    pos: { x: '-10rem', y: '5rem' },
-  },
-  // {
-  //   name: "Numpy",
-  //   icon: <Numpy />,
-  //   experties: 3,
-  //   pos: { x: '-6rem', y: '2rem' },
-  // },
-  // {
-  //   name: "Pandas",
-  //   icon: <Pandas />,
-  //   experties: 3,
-  //   pos: { x: '-8rem', y: '2rem' },
-  // },
-  {
-    name: "Jupyter",
-    icon: <Jupyter />,
-    experties: 4,
-    pos: { x: '-8rem', y: '4rem' },
-  },
-  {
-    name: "Mysql",
-    icon: <Mysql />,
-    experties: 3,
-    pos: { x: '-8rem', y: '0rem' },
-  },
-  {
-    name: "Mongodb",
-    icon: <Mongodb />,
-    experties: 1,
-    pos: { x: '-6rem', y: '0rem' },
-  },
-  {
-    name: "Firebase",
-    icon: <Firebase />,
-    experties: 2,
-    pos: { x: '-6rem', y: '-2rem' },
-  },
+  "arduino",
+  "c",
+  "cplusplus",
+  "css",
+  "firebase",
+  "git",
+  "github",
+  "gitlab",
+  "go",
+  "haskell",
+  "java",
+  "javascript",
+  "jupyter",
+  "mongodb",
+  "mysql",
+  "postgresql",
+  "nextjs",
+  "nodejs",
+  "npm",
+  "numpy",
+  "pandas",
+  "python",
+  "pytorch",
+  "react",
+  "raspberrypi",
+  "rust",
+  "sass",
+  "solidity",
+  "tensorflow",
+  "typescript",
+  "vercel",
+  "prolog",
+  "anaconda",
 ];
