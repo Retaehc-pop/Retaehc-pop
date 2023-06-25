64fc54cd1299f1fb1a9841ed967217ee1cfed872
Retaehc-pop <papop2003@gmail.com>
2023-06-25 15:45:11 +0200

    add highlight to the route

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -2,7 +2,8 @@ import { prisma } from "../../../lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-
+  const { searchParams } = new URL(request.url)
+  const highlight = searchParams.get('highlight')
   try{
     const res = await prisma.project.findMany({
       include:{
@@ -15,6 +16,7 @@ export async function GET(request: NextRequest) {
       },
       where: {
         published: true,
+        hilight: highlight==='1' ? true : false
       },
     });
     const data = await JSON.stringify(res);
