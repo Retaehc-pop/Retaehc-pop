507d6643ae895e488318e14b8122267db8c524d6
Papop Lekhapanyaporn <papop.lekhapanyaporn@gmail.com>
2024-02-07 17:31:19 +0100

    update api

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -5,22 +5,43 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const highlight = searchParams.get('highlight')
   try{
-    const res = await prisma.project.findMany({
-      include:{
-        tags: true,
-        technologies: {
-          select: { 
-            name: true
-          }
+    if (highlight === "1"){
+      const res = await prisma.project.findMany({
+        include: {
+          tags: true,
+          technologies: {
+            select: {
+              name: true,
+            },
+          },
         },
-      },
-      where: {
-        published: true,
-        hilight: highlight==='1' ? true : false
-      },
-    });
-    const data = await JSON.stringify(res);
-    return new NextResponse(data, { status: 200 });
+        where: {
+          published: true,
+          hilight: true,
+        }
+      });
+      const data = await JSON.stringify(res);
+      return new NextResponse(data, { status: 200 });
+    }else{
+      const res = await prisma.project.findMany({
+        include: {
+          tags: true,
+          technologies: {
+            select: {
+              name: true,
+            },
+          },
+        },
+        where: {
+          published: true,
+        },
+      });
+      const data = await JSON.stringify(res);
+      return new NextResponse(data, { status: 200 });
+    }
+      
+    
+    
   }
   catch(err:any){
     return new NextResponse(err, { status: 400 });
