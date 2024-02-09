7c75f57a571975efc291fc7d4f20bbcdd14e76cc
Papop Lekhapanyaporn <papop.lekhapanyaporn@gmail.com>
2024-02-09 21:15:52 +0100

    change nextjs config

--- next.config.js
+++ next.config.js
@@ -3,11 +3,28 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
+
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      {
+        issuer: /\.[jt]sx?$/,
+        use:[]
+      },
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      }
+    );
 
     return config
   },
